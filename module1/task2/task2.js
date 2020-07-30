36508d23e31196060d43c5421725cd325659230c
Andrii Aleksieiev <andrii_aleksieiev@epam.com>
2020-07-30 18:52:53 +0300

    [module1] Add Module1 -> Task2

--- module1/task2/task2.js
+++ module1/task2/task2.js
@@ -1,8 +1,10 @@
 const {dataSource} = require('./data-source');
-const {dataWriter} = require('./data-writer');
+const fileWriter = require('./data-writer');
+
+const fileHandler = fileWriter.init('./resources/result.txt');
 
 dataSource('./resources/example.csv', (data) => {
-        dataWriter(data);
+        return fileWriter.append(fileHandler, data);
     },
     (e) => console.log(`I know what you feel bro, but it's an error! ${e}`),
     () => console.log("Parsing completed")
